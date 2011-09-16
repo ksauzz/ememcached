48c5958e095d9e89bda7829250f3ee730ca9fbc7
ksauzz <ksauzzmsg@gmail.com>
2011-09-16 20:10:15 +0900

    add test

--- src/ememcached.erl
+++ src/ememcached.erl
@@ -4,19 +4,22 @@
 -export([set/2,add/2]).
 -export([get/1]).
 
-%% https://github.com/memcached/memcached/blob/master/doc/protocol.txt
+%% https://raw.github.com/memcached/memcached/master/doc/protocol.txt
+
 %% Storage commands (there are six: "set", "add", "replace", "append" "prepend" and "cas") 
 %% Retrieval commands (there are two: "get" and "gets")
 %% The command "delete" allows for explicit deletion of items
 %% Commands "incr" and "decr"
 
 init() ->
-  ets:new(ememcached,[bag, named_table]).
+  ets:new(ememcached,[bag, named_table]),
+  ok.
 
 %% "set" means "store this data".
 set(Key,Value) ->
   ets:delete(ememcached, Key),
-  ets:insert(ememcached, {Key,Value}).
+  ets:insert(ememcached, {Key,Value}),
+  ok.
 
 %% "add" means "store this data, but only if the server
 %% *doesn't* already hold data for this key".
@@ -28,6 +31,3 @@ add(Key,Value) ->
 
 get(Key) ->
   lists:concat(lists:map(fun({_,V}) -> V end, ets:lookup(ememcached,Key))).
-
--ifdef(TEST).
--endif.
--- test/ememcached_tests.erl
+++ test/ememcached_tests.erl
@@ -1,3 +1,15 @@
 -module(ememcached_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+simple_test() ->
+  ?assertEqual(ok, ememcached:init()),
+
+  ?assertEqual(ok,    ememcached:set("github","hg")),
+  ?assertEqual(ok,    ememcached:set("github","git")),
+  ?assertEqual("git", ememcached:get("github")),
+
+  ?assertEqual(ok,   ememcached:add("bitbucket","hg")),
+  ?assertEqual(ok,   ememcached:add("bitbucket","git")),
+  ?assertEqual("hg", ememcached:get("bitbucket")),
+  
+  ok.
